959a7756801c415090c8d84f4e082ef52f9d4918
Cesar Gonzalez M. <cesar@cesarg.cl>
2026-03-22 15:55:19 -0300

    first commit

---
+++ src/main/java/cl/cesarg/siiproxyHA/SiiproxyHaApplication.java
@@ -0,0 +1,13 @@
+package cl.cesarg.siiproxyHA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiproxyHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiproxyHaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/cesarg/siiproxyHA/SiiproxyHaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cesarg.siiproxyHA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiproxyHaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
